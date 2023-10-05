a15234bf5aca02faf2758c7dd5ff60f300384b78
Ben Wrighton <ben.wrighton@dia.govt.nz>
2023-10-05 01:15:16 +0000

    Basic vite react set-up

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    // colors: {  // Test override blue default color 
+    //   'blue': '#1fb6ff',
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
+
